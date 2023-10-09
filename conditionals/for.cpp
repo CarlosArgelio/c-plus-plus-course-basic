c006289781093fd3e5e4218ab2a683625e578919
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-10-09 16:43:17 -0400

    feat: ciclo for

---
+++ conditionals/for.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int count = 0;
+    cout << "Ingresa un numero" << endl;
+    cin >> count;
+
+    for (int i = 0; i < count; i++)
+    {
+        cout << i << endl;
+    }
+    
+}
