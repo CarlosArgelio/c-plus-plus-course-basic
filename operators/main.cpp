35be6894d2d58fd4a3bcf952b9579e0739d87c25
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-10-09 16:10:07 -0400

    feat: add operatios

---
+++ operators/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int a = 2;
+    int b = 3;
+
+    int sum = a + b;
+    int multiply = a * b;
+    bool greater_than = a > b;
+    bool smaller_than = a < b;
+
+    cout << sum << endl;
+    cout << multiply << endl;
+    cout << greater_than << endl;
+    cout << smaller_than << endl;
+
+}
