a8cd81aa8347988d110b32e0281c12e11ba13b2d
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-10-09 16:17:59 -0400

    feat: add inputs and outputs

---
+++ input_and_output/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int age = 0;
+    cout << "Hello Wold" << endl;
+    cin >> age;
+    cout << "You have " << age << " Years old"<< endl;
+}
