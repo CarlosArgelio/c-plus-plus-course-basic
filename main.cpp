19337c85c195cc8f0226264492d7b48990ee0a48
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-10-09 16:03:07 -0400

    feat: add list

--- main.cpp
+++ main.cpp
@@ -5,7 +5,11 @@ using namespace std;
 int main() {
     int age = 19;
     string name = "Carlos";
-    char lettle = 'A';
+    const char lettle = 'A';
+
+    int list_ages[] = { 18, 19, 20 };
+
     cout << "Hello World my name is " << name << " and i have " << age << " years old" << endl;
-    cout << lettle;
+    cout << lettle << endl;
+    cout << list_ages[0] << endl;
 }
